1ecb53793eb2779bb0a513bada6a38abf5da2119
Varshithagajula <varshithagajula829@gmail.com>
2025-10-25 09:39:28 +0530

    First Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>© 2025 CryptoTrack - All rights reserved.</p>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd":
+        setCurrency({
+          name: "usd",
+          symbol: "$"
+        });
+        break;
+      case "eur":
+        setCurrency({
+          name: "eur",
+          symbol: "€"
+        });
+        break;
+      case "inr":
+        setCurrency({
+          name: "inr",
+          symbol: "₹"
+        });
+        break;
+      default:
+        setCurrency({
+          name: "usd",
+          symbol: "$"
+        });
+    }
+  }
+
+  return (
+    <div className='navbar'>
+      <Link to="/">
+        <img src={logo} alt="Crypto Logo" className='logo'/>
+      </Link>
+        <ul>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/features"><li>Features</li></Link>
+          {/* <Link to="/pricing"><li>Pricing</li></Link>
+          <Link to="/blog"><li>Blog</li></Link> */}
+
+        </ul>
+        <div className="nav-right">
+            <select onChange={currencyHandler}>
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+                <option value="inr">INR</option>
+            </select>
+
+            {/* <Link to="/signup">
+            <button on >SignUp <img src={arrow} alt="Arrow" /></button>
+            </Link> */}
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
